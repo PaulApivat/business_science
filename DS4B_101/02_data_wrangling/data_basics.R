414785ecc86dbebd2ffb4f40fff433279f22eb7e
paulapivat <paul.apivat@gmail.com>
2020-06-28 11:25:53 +0700

    add data_basics.R to data wrangling folder week 2

---
+++ DS4B_101/02_data_wrangling/data_basics.R
@@ -0,0 +1,28 @@
+# DATA BASICS ----
+
+library(tidyverse)
+
+# Data Types ----
+
+?c
+
+a <- c(1,2,3)
+a
+
+a %>% class()
+
+b <- c('low', 'medium', 'high')
+b
+
+b %>% class()
+
+# Data Structure ----
+
+ab_tbl <- tibble(
+    a,
+    b
+)
+
+ab_tbl
+
+read_rds("business_science/DS4B_101/00_data/bike_sales/data_wrangled/bike_orderlines.rds")
