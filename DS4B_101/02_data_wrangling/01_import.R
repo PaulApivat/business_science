544b64b7d284921b9be1690537d3eac58e408f69
paulapivat <paul.apivat@gmail.com>
2020-06-28 12:46:31 +0700

    add importing csv files to 01_import.R

--- DS4B_101/02_data_wrangling/01_import.R
+++ DS4B_101/02_data_wrangling/01_import.R
@@ -20,8 +20,20 @@ library(RSQLite)
 # 2.0 readr ----
 
 # 2.1 CSV ----
+bike_orders_csv_tbl <- readr::read_csv("../00_data/bike_sales/data_wrangled/bike_orderlines.csv")
 
+readr::problems(bike_orders_csv_tbl)
 
+# filter rows by slice() function
+bike_orders_csv_tbl %>%
+    slice(7916)
+
+# converting order_id column from integer to double using col_double() function
+read_csv("../00_data/bike_sales/data_wrangled/bike_orderlines.csv",
+         col_types = cols(
+             order_id = col_double()
+         )) %>%
+    slice(7916)
 
 # 2.2 RDS ----
 
