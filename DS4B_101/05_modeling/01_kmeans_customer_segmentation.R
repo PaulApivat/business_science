8e289bae60d72102cde71a647ccdce0980473d39
paulapivat <paul.apivat@gmail.com>
2020-07-31 15:15:35 +0700

    add visualization of UMAP Projection with K-Means Cluster Assignment of Customers

--- DS4B_101/05_modeling/01_kmeans_customer_segmentation.R
+++ DS4B_101/05_modeling/01_kmeans_customer_segmentation.R
@@ -135,13 +135,74 @@ kmeans_mapped_tbl %>%
 
 # 3.1 Use UMAP to get 2-D Projection ----
 
+# NOTE: UMAP is similar to PCA; a dimensionality reduction technique that captures the structure
+# of a high-dimension data set (many numeric columns, like customer_product_tbl) in a 
+# two column (x and y) data set
+
+# NOTE: each Umap project involves randomness so the plots might look different
+?umap
+
+umap_obj <- customer_product_tbl %>%
+    select(-bikeshop_name) %>%
+    umap()
+
+umap_results_tbl <- umap_obj$layout %>%
+    as_tibble() %>%
+    set_names(c("x", "y")) %>%
+    bind_cols(
+        customer_product_tbl %>% select(bikeshop_name)
+    )
+
+umap_results_tbl %>%
+    ggplot(aes(x = x, y = y)) +
+    geom_point() +
+    geom_label_repel(aes(label = bikeshop_name), size = 3)
 
 # 3.2 Use K-Means to Add Cluster Assignments ----
 
+umap_results_tbl
+
+kmeans_4_obj <- kmeans_mapped_tbl %>%
+    # pull the 'list' of k-means objects
+    pull(k_means) %>%
+    pluck(4)
+
+kmeans_4_clusters_tbl <- kmeans_4_obj %>% 
+    augment(customer_product_tbl) %>%
+    select(bikeshop_name, .cluster)
+
+# join with umap_results
+
+umap_kmeans_4_results_tbl <- umap_results_tbl %>%
+    left_join(kmeans_4_clusters_tbl)
+
+
+# alternative
+kmeans_mapped_tbl %>%
+    filter(centers == 4) %>%
+    pull(k_means) %>%
+    pluck(1)
 
 # 3.3 Visualize UMAP'ed Projections with Cluster Assignments ----
 
-
+umap_kmeans_4_results_tbl %>%
+    mutate(label_text = str_glue("Customer: {bikeshop_name}
+                                 Cluster: {.cluster}")) %>%
+    ggplot(aes(x = x, y = y, color = .cluster)) +
+    
+    # Geometries
+    geom_point() + 
+    geom_label_repel(aes(label = label_text), size = 3) +
+    
+    # Formatting
+    theme_tq() + 
+    scale_color_tq() +
+    labs(
+        title = 'Customer Segmentation: 2D Projection',
+        subtitle = 'UMAP 2D Projection with K-Means Cluster Assignment',
+        caption = 'Conclusion: 4 Customer Segments identified using 2 algorithms'
+    ) +
+    theme(legend.position = 'none')
 
 
 # 4.0 ANALYZE PURCHASING TRENDS ----
