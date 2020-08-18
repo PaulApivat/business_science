a7af1cf1f47155b9c99d19d5a2f6f93346fb8d8d
paulapivat <paul.apivat@gmail.com>
2020-08-18 19:58:26 +0700

    load package, standardize data, perform cross-validation

--- DS4B_101/05_modeling/linear_model_pkg_comparison.R
+++ DS4B_101/05_modeling/linear_model_pkg_comparison.R
@@ -1,3 +1,35 @@
 # Linear Models Package Comparison
 
 # Source: https://www.datacamp.com/community/tutorials/tutorial-ridge-lasso-elastic-net
+
+# Load Libraries ----
+
+set.seed(123) # seed for reproducibility
+library(glmnet) # for ridge regression
+library(dplyr) # for data cleaning
+
+data('mtcars')
+
+# Standardize x and y via scale() function ----
+y <- mtcars %>%
+    select(mpg) %>%
+    scale(center = TRUE, scale = FALSE) %>%
+    as.matrix()
+
+x <- mtcars %>%
+    select(-mpg) %>%
+    as.matrix()
+
+# they are both matrices
+class(x)
+class(y)
+
+# Perform 10-fold cross-validation to select lambda ----
+
+lambdas_to_try <- 10^seq(-3, 5, length.out = 100)
+ 
+class(lambdas_to_try)
+
+# Setting Alpha = 0 implements Ridge Regression ----
+
+
